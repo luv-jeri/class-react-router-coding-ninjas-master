b60eab4860c002371d9be19c8e301d3732ef97d5
ankur0207 <ankursharma0207@gmail.com>
2023-01-01 20:34:43 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Hero from './pages/app/hero/Hero';
 import Nav from './components/nav/Nav';
 import Courses from './pages/app/courses/Courses';
+
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Nav />
-      <Hero />
-      <Courses />
+
+      <Routes>
+        <Route path='/' element={<Hero />} />
+        <Route path='courses' element={<Courses />} />
+      </Routes>
     </div>
   );
 }
